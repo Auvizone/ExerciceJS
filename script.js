9553fd37716f6617c5e8ec5e8d6636c649d6c6b8
Auvizone <humphrey.preston11@gmail.com>
2020-08-31 12:20:55 +0200

    ajout création div boucle

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ fetch('data.json')
         var i;
         for( i = 0; i < data.length; i++) {
             console.log('id' + i)
+            document.createElement('id'+ i);
             document.getElementById('id'+ i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
-            document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
-            document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
+            // document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
+            // document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
         }
     });
