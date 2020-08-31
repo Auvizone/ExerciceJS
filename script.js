c899c856c2528eb94c2181340c178e5460adea95
Auvizone <humphrey.preston11@gmail.com>
2020-08-31 12:18:04 +0200

    modif en boucle

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        document.getElementById('id1').innerHTML = '<br>Prenom:' + data[0].nom + '<br> Pseudo: ' + data[0].pseudo + '<br> Xp:' + data[0].xp + ('<br><img src="'+data[0].urlavatar+'"/><br>');
-        document.getElementById('id2').innerHTML = '<br>Prenom:' + data[1].nom + '<br> Pseudo: ' + data[1].pseudo + '<br> Xp:' + data[1].xp + ('<br><img src="'+data[1].urlavatar+'"/><br>');
-        document.getElementById('id3').innerHTML = '<br>Prenom:' + data[2].nom + '<br> Pseudo: ' + data[2].pseudo + '<br> Xp:' + data[2].xp + ('<br><img src="'+data[2].urlavatar+'"/><br>');
+        var i;
+        for( i = 0; i < data.length; i++) {
+            console.log('id' + i)
+            document.getElementById('id'+ i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
+            document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
+            document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
+        }
     });
