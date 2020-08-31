81d6dae3bb5593ca2cd98e22411e97a69d965cf7
Auvizone <humphrey.preston11@gmail.com>
2020-08-31 12:34:56 +0200

    ajout création div dans body

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ fetch('data.json')
     .then(data => {
         var i;
         for( i = 0; i < data.length; i++) {
-            console.log('id' + i)
-            document.createElement('id'+ i);
+            var iDiv = document.createElement('div');
+            iDiv.id = ('id'+ i);
+            document.getElementById('body').appendChild(iDiv);
             document.getElementById('id'+ i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
-            // document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
-            // document.getElementById('id' + i).innerHTML = '<br>Prenom:' + data[i].nom + '<br> Pseudo: ' + data[i].pseudo + '<br> Xp:' + data[i].xp + ('<br><img src="'+data[i].urlavatar+'"/><br>');
         }
     });
